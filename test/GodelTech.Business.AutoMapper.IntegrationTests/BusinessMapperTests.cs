c500558f1a2113d49422939a88627fcc8b61466e
Andrei Yaskevich <andrez.yaskevich@gmail.com>
2022-04-04 20:54:41 +0300

    clear usings

--- test/GodelTech.Business.AutoMapper.IntegrationTests/BusinessMapperTests.cs
+++ test/GodelTech.Business.AutoMapper.IntegrationTests/BusinessMapperTests.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
 using GodelTech.Business.AutoMapper.IntegrationTests.Fakes;
 using AutoMapper;
 using Xunit;
